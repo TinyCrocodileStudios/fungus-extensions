e5ae4c774d64859a2fa1e44136689838ca110d33
Tiny Crocodile <53369481+TinyCrocodileStudios@users.noreply.github.com>
2019-07-27 21:04:44 +0200

    added my scripts

---
+++ AutoScrollTheScrollRectBy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;  // Required when Using UI elements.
+using Fungus;
+
+[CommandInfo ("Tiny Crocodile Studios", 
+    "Scroll To Position By",
+    "Scroll the Scroll Rect by x percent.")]
+
+public class AutoScrollTheScrollRectBy : Command
+{
+
+    public float scrollPercentageStep;
+    public ScrollRect myScrollRect;
+
+    public override void OnEnter()
+    {
+        float scrollPercentage = myScrollRect.verticalNormalizedPosition;
+
+        //scrolls the content in a scroll rect by a certain percentage
+        scrollPercentage -= scrollPercentageStep;
+        if(scrollPercentage < 0)
+            scrollPercentage = 0;
+
+        if(scrollPercentage > 100)
+            scrollPercentage = 100;
+
+        myScrollRect.verticalNormalizedPosition = scrollPercentage;  
+        //to do: make this more smooth by increasing the scroll value slowly
+
+        Continue();  
+    }
+
+}
---
+++ SetCursorVisibility.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+[CommandInfo("Tiny Crocodile Studios",
+            "Set Cursor Visibility",
+            "Set the visibility for the cursor.")]
+
+public class SetCursorVisibility : Command
+{
+
+    public bool visibility = true;
+
+    public override void OnEnter()
+    {
+        Cursor.visible = visibility;
+        Continue();
+    }
+
+    /*public override string GetSummary()
+    {
+        return "";
+    }*/
+
+}
