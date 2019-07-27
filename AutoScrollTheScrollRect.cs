95326b0c2a672cab598ae1abce2ac2a7c433bce0
Tiny Crocodile <53369481+TinyCrocodileStudios@users.noreply.github.com>
2019-07-27 21:17:59 +0200

    renamed scroll file

---
+++ AutoScrollTheScrollRect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;  // Required when Using UI elements.
+using Fungus;
+
+[CommandInfo ("Tiny Crocodile Studios", 
+    "Scroll To Position",
+    "Scroll the Scroll Rect to certain position.")]
+
+public class AutoScrollTheScrollRecht : Command
+{
+
+    public float scrollPercentage;
+    public ScrollRect myScrollRect;
+
+    //scrolls to a certain position in a scroll rect, defined by percentage
+    public override void OnEnter()
+    {
+        myScrollRect.verticalNormalizedPosition = scrollPercentage;  
+        Continue();  
+    }
+
+
+}
